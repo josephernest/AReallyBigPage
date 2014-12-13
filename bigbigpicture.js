10a78ae0f18c646e5d0886c49b04b87e20a77489
josephernest <nouvellecollection@gmail.com>
2014-12-13 13:59:27 +0100

    Keep history

--- bigbigpicture.js
+++ bigbigpicture.js
@@ -7,9 +7,11 @@ server.listen(3001);
 
 app.use(express.static(__dirname + '/public'));
 
-var id = 1;
+var id = 1,
+  texts = {};
 
 io.on('connection', function (socket) {
+  for (var textId in texts) { socket.emit('text', texts[textId]); } // when a new user connects, send all past history
   socket.on('temp id', function (data) { socket.emit('new id', {tempId: data.tempId, id: id++}); }); // to sender only
-  socket.on('text', function (data) { socket.broadcast.emit('text', data);  }); // to all except sender 
+  socket.on('text', function (data) { socket.broadcast.emit('text', data); texts[data.id] = data;  }); // to all except sender 
 });
