b3fc10925e41e808e3111486ab3f17f8c566b7c7
root <root@bigpanda.(none)>
2017-11-17 07:54:00 -0500

    Added n users online

--- areallybigpage.js
+++ areallybigpage.js
@@ -10,12 +10,41 @@ app.use(express.static(__dirname + '/public'));
 
 var jsonfile = require('./areallybigpage.json');
 var id = jsonfile['lastid'];
-var texts = jsonfile['texts'];
+var db = jsonfile['texts'];
+var userCount = 0;
 
-setInterval(function() { fs.writeFile('./areallybigpage.json', JSON.stringify({ 'lastid': id, 'texts': texts })); }, 60 * 1000); // serialize to disk every minute
+for (var textId in db) {
+    if (db[textId].text.trim().length == 0  || db[textId].id.indexOf('tempId') > -1) { 
+        delete db[textId]; 
+    } 
+}
+
+function serializeTexts() {
+    fs.writeFile('./areallybigpage.json', JSON.stringify({ 'lastid': id, 'texts': db }, null, 2)); 
+}
+
+setInterval(serializeTexts, 60 * 1000); // serialize to disk every minute
 
 io.on('connection', function (socket) {
-  for (var textId in texts) { socket.emit('text', texts[textId]); } // when a new user connects, send all past history
-  socket.on('temp id', function (data) { socket.emit('new id', {tempId: data.tempId, id: id++}); }); // to sender only
-  socket.on('text', function (data) { socket.broadcast.emit('text', data); texts[data.id] = data;  }); // to all except sender 
+    userCount ++; 
+    io.sockets.emit('usercount', userCount);
+  
+    for (var textId in db) { socket.emit('text', db[textId]); } // when a new user connects, send all past history
+
+    socket.on('temp id', function (data) { 
+        socket.emit('new id', { tempId: data.tempId, id: id++ });       // to sender only
+    }); 
+
+    socket.on('text', function (data) { 
+        if (data.text.length > 0  && data.text.length < 2048 && data.id.indexOf('tempId') == -1) { 
+            db[data.id] = data;
+            socket.broadcast.emit('text', data); // to all except sender 
+        }
+        else { 
+            delete db[data.id]; 
+        }
+    });
+
+    socket.on('disconnect', function () { userCount--; socket.broadcast.emit('usercount', userCount); });
+
 });
