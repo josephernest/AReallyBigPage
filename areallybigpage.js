0d9299f6f095aa0ea7e77104f2e46ea3b6ecfcac
josephernest <nouvellecollection@gmail.com>
2018-04-06 17:10:31 +0200

    Update areallybigpage.js

--- areallybigpage.js
+++ areallybigpage.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io = require('socket.io')({ path: '/wsareallybigpage'}).listen(server);
 var fs = require('fs');
 
 server.listen(3001);
